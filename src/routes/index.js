fbfc8d5227869d30ba5d3b5c60393022e488b883
marco perez <mp4667596@gmail.com>
2023-11-06 07:55:09 -0300

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const genreRouter = require('./genre.routers');
+const actorRouter = require('./actor.routers');
+const directorRouter = require('./director.router');
+const movieRouter = require('./movie.router');
 const router = express.Router();
 
+
 // colocar las rutas aquí
+router.use('/genres', genreRouter)
+router.use('/actors', actorRouter)
+router.use('/directors', directorRouter)
+router.use('/movies', movieRouter)
+
+
 
 
 module.exports = router;
